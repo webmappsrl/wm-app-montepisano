d63d416c75917dcd1fd2cf6de0b38c7685bac446
Luca Baroncini <baroncini@netseven.it>
2017-07-14 13:18:39 +0200

    bug fixed caricamento pagine

--- bare/www/core/js/components/custom/custom.controller.js
+++ bare/www/core/js/components/custom/custom.controller.js
@@ -45,20 +45,19 @@ angular.module('webmapp')
     };
 
     var key = CONFIG.OFFLINE.pagesUrl + currentPageType + '.html';
-    MapService.getPageInPouchDB(key).then(function(rsp){
+    MapService.getPageInPouchDB(key).then(function(rsp) {
         var html = rsp.data;
         console.log(html);
-        $templateRequest(templateUrl).then(function() {
-            var openFn = vm.isBrowser ? 'vm.openInAppBrowser' : 'vm.openInExternalBrowser';
 
+        var openFn = vm.isBrowser ? 'vm.openInAppBrowser' : 'vm.openInExternalBrowser';
 
-            console.log(html);
+        console.log(html);
 
-            vm.body = html.replace(/href="([^\'\"]+)/g, 'ng-click="' + openFn + '(\'$1\')" href=""');
+        vm.body = html.replace(/href="([^\'\"]+)/g, 'ng-click="' + openFn + '(\'$1\')" href=""');
 
-        }, function() {
-            console.log('Template retrive error', currentPageType);
-        });
+    }, function() {
+        console.log('Template retrive error', currentPageType);
+
     });
 
 
