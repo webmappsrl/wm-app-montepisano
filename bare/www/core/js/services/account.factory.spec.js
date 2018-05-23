1e812d6cedca505997088d47acacec674223a95f
Marco Zizi <zizimarcoz@gmail.com>
2018-05-23 10:14:38 +0200

    Adding test

--- bare/www/core/js/services/account.factory.spec.js
+++ bare/www/core/js/services/account.factory.spec.js
@@ -19,15 +19,16 @@ describe('Account.Factory Test', function() {
     describe('Account.Factory.login', function() {
 
         it('it should execute login successfully', function() {
-            var fakeUser = 'fakeUser';
-            var fakePass = 'fakePass';
-            var data = {
-                user: fakePass,
-                pass: fakeUser
-            };
+            // var fakeUser = 'fakeUser';
+            // var fakePass = 'fakePass';
+            // var data = {
+            //     user: fakePass,
+            //     pass: fakeUser
+            // };
 
-            // $httpBackend.whenGET().respond(200);
-            // $httpBackend.when().respond(200, ["ResponseOK"]);
+            // $httpBackend.whenGET().respond(404);
+            // console.log(accountService.baseUrl + accountService.endpoint + 'user/');
+            // $httpBackend.when('POST', accountService.baseUrl + accountService.endpoint + 'user/').respond(200, ["ResponseOK"]);
             // accountService.login(fakeUser, fakePass).then(function(response) {
 
             //     expect(response.data.lenght).toBe('ResponseOK');
