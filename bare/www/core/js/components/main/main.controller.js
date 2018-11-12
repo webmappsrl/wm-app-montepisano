20946968d341d9f77cf61429bad719cfa871275f
Davide Pizzato <pizzatodavide@hotmail.it>
2018-11-12 11:45:01 +0100

    Added logo to bottom-center in the map view

--- bare/www/core/js/components/main/main.controller.js
+++ bare/www/core/js/components/main/main.controller.js
@@ -26,8 +26,6 @@ angular.module('webmapp')
         var overlaysGroupMap = Model.getOverlaysGroupMap(),
             overlayMap = Model.getOverlaysMap();
 
-        var previousBounds = null;
-
         var hideExpanderInDetails = CONFIG.OPTIONS.hideExpanderInDetails;
 
         var shareScope = $rootScope.$new(),
@@ -79,10 +77,9 @@ angular.module('webmapp')
         Utils.createModal('core/js/modals/shareModal.html', {
             backdropClickToClose: true,
             hardwareBackButtonClose: true
-        }, shareScope)
-            .then(function (modal) {
-                shareModal = modal;
-            });
+        }, shareScope).then(function (modal) {
+            shareModal = modal;
+        });
 
         shareScope.vm = {};
         shareScope.vm.textblock = '';
@@ -136,7 +133,7 @@ angular.module('webmapp')
         vm.hideDeactiveCentralPointer = CONFIG.OPTIONS && CONFIG.OPTIONS.hideDeactiveCentralPointer;
         vm.toggleMapInDetails = CONFIG.OPTIONS && CONFIG.OPTIONS.toggleMapInDetails;
 
-        vm.isCoordsBlockExpanded = true;
+        vm.isCoordsBlockExpanded = CONFIG.OPTIONS.coordsNotExpanded ? false : true;
 
         vm.navigationAvailable = false;
         vm.isNavigating = false;
@@ -181,9 +178,10 @@ angular.module('webmapp')
         vm.showRightMenu = false;
         vm.filterIcon = CONFIG.OPTIONS.filterIcon;
 
-        $scope.$watch(function () {
-            return $ionicSideMenuDelegate.isOpenLeft();
-        },
+        $scope.$watch(
+            function () {
+                return $ionicSideMenuDelegate.isOpenLeft();
+            },
             function (isOpen) {
                 if (isOpen) {
                     vm.showRightMenu = false;
@@ -954,6 +952,48 @@ angular.module('webmapp')
                 if (window !== top) {
                     MapService.disableWheelZoom();
                 }
+            }
+
+            if (CONFIG.OPTIONS.mapLogoUrl) {
+                console.log("ok")
+                vm.mapLogoUrl = CONFIG.OPTIONS.mapLogoUrl;
+
+                var updateLogo = function (url) {
+                    Communication.get(url).then(function (response) {
+                        var urlCreator = window.URL || window.webkitURL;
+
+                        vm.mapLogoUrl = urlCreator.createObjectURL(response);
+
+                        var reader = new FileReader();
+                        reader.readAsDataURL(response);
+                        reader.onloadend = function () {
+                            vm.mapLogoUrl = reader.result;
+                            MapService.setItemInLocalStorage("$wm_mapLogo", JSON.stringify(
+                                {
+                                    url: url,
+                                    base64: vm.mapLogoUrl
+                                }
+                            ));
+
+                            console.log("updated")
+                        }
+                    }, function (err) {
+                        console.log("error download")
+                    });
+                };
+
+                MapService.getItemFromLocalStorage("$wm_mapLogo").then(function (saved) {
+                    var data = JSON.parse(saved.data);
+                    if (CONFIG.OPTIONS.mapLogoUrl !== data.url) {
+                        updateLogo(CONFIG.OPTIONS.mapLogoUrl);
+                    }
+                    else {
+                        vm.mapLogoUrl = data.base64;
+                        console.log("cached")
+                    }
+                }, function (err) {
+                    updateLogo(CONFIG.OPTIONS.mapLogoUrl);
+                });
             }
 
             // Note: route's first argument can take any kind of object as its data,
