c8e4bfb9d28724a7de285e5a7af9ab0ab85518be
Davide Pizzato <pizzatodavide@hotmail.it>
2018-12-10 11:36:34 +0100

    Added plugin and function to activate it (WIP)

--- bare/www/core/js/services/map.factory.js
+++ bare/www/core/js/services/map.factory.js
@@ -106,6 +106,30 @@ angular.module('webmapp')
             iconAnchor: [20, 20],
         });
 
+        var elevationControl = L.control.elevation({
+            position: "bottomleft",
+            theme: "steelblue-theme", //default: lime-theme
+            width: 600,
+            height: 125,
+            margins: {
+                top: 10,
+                right: 20,
+                bottom: 30,
+                left: 50
+            },
+            useHeightIndicator: true, //if false a marker is drawn at map position
+            interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
+            hoverNumber: {
+                decimalsX: 3, //decimals on distance (always in km)
+                decimalsY: 0, //deciamls on hehttps://www.npmjs.com/package/leaflet.coordinatesight (always in m)
+                formatter: undefined //custom formatter function may be injected
+            },
+            xTicks: undefined, //number of ticks in x axis, calculated by default according to width
+            yTicks: undefined, //number of ticks on y axis, calculated by default according to height
+            collapsed: true,  //collapsed mode, show chart on click or mouseover
+            imperial: false    //display imperial units instead of metric
+        });
+
         var db = new PouchDB('webmapp');
 
         var userTrackPolyline = null;
@@ -2022,6 +2046,12 @@ angular.module('webmapp')
                     circleLocation.accuracy = null;
                 }
             }
+        };
+
+        mapService.toggleElevationControl = function (data) {
+
+
+            elevationControl.addTo(map);
         };
 
         mapService.isInBoundingBox = function (lat, long) {
